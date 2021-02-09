fa1c12c5c84015d284a6796e5e3c1d7de132e67f
Tjelvar Olsson <olssont@nbi.ac.uk>
2021-02-09 22:32:14 +0000

    Add /annotation_filter_plugin/keys route

--- dtool_lookup_server_annotation_filter_plugin/__init__.py
+++ dtool_lookup_server_annotation_filter_plugin/__init__.py
@@ -1,4 +1,23 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    abort,
+    request,
+    jsonify,
+)
+
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity,
+)
+
+from dtool_lookup_server import (
+    AuthenticationError
+)
+
+from dtool_lookup_server_annotation_filter_plugin.utils import (
+    get_annotation_key_info_by_user,
+)
+
 
 annotation_filter_bp = Blueprint(
     'annotation_filter_plugin',
@@ -8,5 +27,12 @@ annotation_filter_bp = Blueprint(
 
 
 @annotation_filter_bp.route('/keys', methods=["POST"])
+@jwt_required
 def keys():
-    return {"color": 2, "pattern": 2}
+    username = get_jwt_identity()
+    query = request.get_json()
+    try:
+        data = get_annotation_key_info_by_user(username, query)
+    except AuthenticationError:
+        abort(401)
+    return jsonify(data)
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,7 +1,7 @@
 import json
 
 from . import tmp_app  # NOQA
-from . import GRUMPY_TOKEN
+from . import GRUMPY_TOKEN, SLEEPY_TOKEN, NOONE_TOKEN
 
 
 def test_keys_route_all(tmp_app):  # NOQA
@@ -17,3 +17,93 @@ def test_keys_route_all(tmp_app):  # NOQA
     content = json.loads(r.data.decode("utf-8"))
     expected_content = {"color": 2, "pattern": 2}
     assert content == expected_content
+
+
+def test_keys_route_with_key_filter(tmp_app):  # NOQA
+    headers = dict(Authorization="Bearer " + GRUMPY_TOKEN)
+    data = {"annotation_keys": ["color"]}
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 200
+    content = json.loads(r.data.decode("utf-8"))
+    expected_content = {"color": 2, "pattern": 1}
+    assert content == expected_content
+
+
+def test_keys_route_with_annotation_filter(tmp_app):  # NOQA
+    headers = dict(Authorization="Bearer " + GRUMPY_TOKEN)
+    data = {"annotations": {"pattern": "stripey"}}
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 200
+    content = json.loads(r.data.decode("utf-8"))
+    expected_content = {"pattern": 1}
+    assert content == expected_content
+
+
+def test_keys_route_with_complex_filter(tmp_app):  # NOQA
+    headers = dict(Authorization="Bearer " + GRUMPY_TOKEN)
+
+    data = {
+        "annotation_keys": ["pattern"],
+        "annotations": {"pattern": "stripey"}
+    }
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 200
+    content = json.loads(r.data.decode("utf-8"))
+    expected_content = {"pattern": 1}
+    assert content == expected_content
+
+    data = {
+        "annotation_keys": ["color"],
+        "annotations": {"pattern": "stripey"}
+    }
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 200
+    content = json.loads(r.data.decode("utf-8"))
+    expected_content = {}
+    assert content == expected_content
+
+
+def test_keys_route_with_sleepy_user(tmp_app):  # NOQA
+    headers = dict(Authorization="Bearer " + SLEEPY_TOKEN)
+    data = {}
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 200
+    content = json.loads(r.data.decode("utf-8"))
+    assert content == {}
+
+
+def test_keys_route_with_noone_user(tmp_app):  # NOQA
+    headers = dict(Authorization="Bearer " + NOONE_TOKEN)
+    data = {}
+    r = tmp_app.post(
+        "/annotation_filter_plugin/keys",
+        headers=headers,
+        data=json.dumps(data),
+        content_type="application/json"
+    )
+    assert r.status_code == 401
